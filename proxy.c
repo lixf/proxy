06264b233799554d3fe9737e0e04ea5838e527ff
Xiaofan Li <lixf@cmu.edu>
2013-11-27 16:50:28 -0500

    very broken

--- proxy.c
+++ proxy.c
@@ -13,7 +13,6 @@
 /* Recommended max cache and object sizes */
 #define MAX_CACHE_SIZE 1049000
 #define MAX_OBJECT_SIZE 102400
-#define MAXLINE 1024
 #define MAXHDR 2048
 
 /* You won't lose style points for including these long lines in your code */
@@ -24,18 +23,22 @@ static const char *connction = "Connection: close\r\n";
 static const char *pconnection = "Proxy-Connection: close\r\n";
 
 
+void parseReq (int connfd,char* newReq,char* reqhdr,char* host);
+char* read_reqhdrs(rio_t *rp,char* reqhdr,char* host);
+char* geturlHost(char* url, char* host);
+void makeReq(char* host,char* newReq,char* reqheader,char* response);
+
+
 int main(int argc,char** argv){
     
     int listenfd, connfd, portSelf, clientlen;
-    
-    char reqBuf[MAX_REQUEST_SIZE];
+    //char reqBuf[MAXLINE];
     //char response[MAX_OBJECT_SIZE];
     
     struct sockaddr_in clientaddr;
     struct hostent *hp;
     char *haddrp; //host addr pointer a string
 
-    rio_t rio;
 
     printf("%s%s%s", user_agent, acceptHdr, accept_encoding);
     
@@ -52,35 +55,41 @@ int main(int argc,char** argv){
     
     //prefork threads
     //check cache
+    
     while (1) {
-      //wait for connection
-      clientlen = sizeof(clientaddr);
-      connfd = Accept(listenfd, (SA *)&clientaddr, (socklen_t*)&clientlen);
-      
-      /* Determine the domain name and IP address of the client */
-      hp = Gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr,
-            sizeof(clientaddr.sin_addr.s_addr), AF_INET);
-      haddrp = inet_ntoa(clientaddr.sin_addr);
-      printf("server connected to %s (%s)\n", hp->h_name, haddrp);
-      
-      printf("now parsing request\n");
-      
-      //allocate mem for the reqest and the header
-      char* reqheader = malloc(MAXHDR);
-      char* newReq = malloc(MAXLINE);
-      char* host = malloc(MAXLINE);
-      char* response = malloc(MAX_OBJECT_SIZE);
-
-      //parse the inputs and put it in the allocated space
-      parseReq(connfd,newReq,reqheader,host);
-      
-      //make new request
-      makeReq(host,newReq,reqheader,response);
-      
-      //return the content to the client
-      Rio_writen(connfd,response, strlen(reqponse));
-
-      Close(connfd);
+        //wait for connection
+        clientlen = sizeof(clientaddr);
+        connfd = Accept(listenfd, (SA *)&clientaddr, (socklen_t*)&clientlen);
+        
+        /* Determine the domain name and IP address of the client */
+        hp = Gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr,
+              sizeof(clientaddr.sin_addr.s_addr), AF_INET);
+        haddrp = inet_ntoa(clientaddr.sin_addr);
+        printf("proxy connected to %s (%s)\n", hp->h_name, haddrp);
+        
+        printf("now parsing request\n");
+        
+        //allocate mem for the reqest and the header
+        char* reqhdr = (char*) malloc(MAXHDR+1);
+        char* newReq = (char*) malloc(MAXLINE+1);
+        char* host = (char*) malloc(MAXLINE+1);
+        char* response = (char*) malloc(MAX_OBJECT_SIZE+1);
+        
+        reqhdr = "";
+        newReq = "";
+        host = "";
+        response = "";
+        //parse the inputs and put it in the allocated space
+        parseReq(connfd,newReq,reqhdr,host);
+        
+        //make new request
+        makeReq(host,newReq,reqhdr,response);
+        
+        //return the content to the client
+        Rio_writen(connfd,response, strlen(response));
+        
+        printf("finsihed wirting\n");
+        Close(connfd);
     }
     //forward request to server*
     //get result and forward to client
@@ -88,24 +97,32 @@ int main(int argc,char** argv){
     return 0;
 }
 
-void parseReq (int connfd,newReq,reqheader,host) {
+void parseReq (int connfd,char* newReq,char* reqhdr,char* host) {
     //int is_static;
     struct stat sbuf;
     char buf[MAXLINE], method[MAXLINE], url[MAXLINE], version[MAXLINE];
     char filename[MAXLINE], cgiargs[MAXLINE];
-    char oldhost[MAXLINE];
+    char* oldHost;
     rio_t rio;
 
     Rio_readinitb(&rio,connfd);
     Rio_readlineb(&rio, buf, MAXLINE);
     sscanf(buf, "%s %s %s", method, url, version);
-    
+
     //takes care of the header and returns the host header
     //from the request 
-    oldHost = read_requesthdrs(&rio,reqheader);
+    oldHost = read_reqhdrs(&rio,reqhdr,host);
 
     //write the new request
-    if *oldHost != 
+    if (strcmp(oldHost,"")){
+        //if can't find host in hdr and the url, return error msg
+        oldHost = geturlHost(url,host);
+        if(strcmp(oldHost,"")){
+            char* retErr = "bad request no host\n";
+            Rio_writen(&rio,retErr,strlen(retErr));
+            exit(1);
+        }
+    }
 
     return;
 
@@ -114,14 +131,26 @@ void parseReq (int connfd,newReq,reqheader,host) {
 /* This function reads the header and returns to parseReq
  * and determine what should be ignored and what should be forwarded
  */
-char* read_requesthdrs(rio_t *rp){
-    char buf[MAXHDR];
-    
-    Rio_readlineb(rp, buf, MAXHDR);
-    while(strcmp(buf, "\r\n")) {
-        Rio_readlineb(rp, buf, MAXHDR);
+char* read_reqhdrs(rio_t *rp,char* reqhdr,char* host){
+    //first read in all the headers
+    Rio_readlineb(rp, reqhdr, MAXHDR);
+    while(strcmp(reqhdr, "\r\n")) {
+        Rio_readlineb(rp, reqhdr, MAXHDR);
+        printf("%s", reqhdr);
     }
-    return buf;
+   
+    host = strstr(reqhdr,"host:");
+    //it contains a null char
+    host += 5;
+
+    return host;
+}
+
+char* geturlHost(char* url, char* host){
+    char* end = strstr(url,"/");
+    host = end;
+    return host;
+
 }
 
 
@@ -131,11 +160,11 @@ char* read_requesthdrs(rio_t *rp){
  * detects how big the response is and write \0 at the end
  */
 
-void makeReq(host,newReq,reqheader,response){
+void makeReq(char* host,char* newReq,char* reqheader,char* response){
     
     int clientfd, port;
-    char *host;
     rio_t rio;
+    const char* endStr = "\r\n"; 
 
     port = atoi("80");
     
@@ -145,8 +174,11 @@ void makeReq(host,newReq,reqheader,response){
     //write the request
     Rio_writen(clientfd, newReq, strlen(newReq));
     Rio_writen(clientfd, reqheader, strlen(reqheader));
+    Rio_writen(clientfd,endStr,strlen(endStr));
     
     Rio_readlineb(&rio, response, MAX_OBJECT_SIZE);
+    
+    printf("response: %s\n",response);
 
     Close(clientfd);
 }
